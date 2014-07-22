ec6a23032cead9aea3931b81f5851a334f0fffb2
Naresh Jain <nashjain@gmail.com>
2014-07-22 20:59:27 +0530

    minor refactoring to clean up the code

--- src/main/java/assembly/Graph.java
+++ src/main/java/assembly/Graph.java
@@ -6,59 +6,60 @@ import java.util.ArrayList;
 
 /**
  * @author dev
- * 
  * @param <E>
  */
 public class Graph<E> {
 
-  /**
-   * ArrayList to hold edges for random access
-   */
-  private ArrayList<Edge<E>> edges;
+	/**
+	 * ArrayList to hold edges for random access
+	 */
+	private ArrayList<Edge<E>> edges;
 
-  public Graph() {
-    edges = new ArrayList<Edge<E>>();
-  }
+	public Graph() {
+		edges = new ArrayList<Edge<E>>();
+	}
 
-  /**
-   * @param vertex
-   * @return - boolean
-   * 
-   *         Add Edge to the List
-   */
-  public boolean addEdge(Edge<E> vertex) {
-    if (edges.contains(vertex))
-      return false;
-    edges.add(vertex);
-    return true;
-  }
+	/**
+	 * @param vertex
+	 * @return - boolean Add Edge to the List
+	 */
+	public boolean addEdge(Edge<E> vertex) {
+		if (edges.contains(vertex))
+			return false;
+		edges.add(vertex);
+		return true;
+	}
 
-  public boolean contains(Edge<E> vertex) {
-    return edges.contains(vertex);
-  }
+	public boolean contains(Edge<E> vertex) {
+		return edges.contains(vertex);
+	}
 
-  public Edge<E> get(int index) {
-    return edges.get(index);
-  }
+	public Edge<E> get(int index) {
+		return edges.get(index);
+	}
 
-  /**
-   * @return : number of Edges in Graph
-   */
-  public int count() {
-    return edges.size();
-  }
+	/**
+	 * @return : number of Edges in Graph
+	 */
+	public int count() {
+		return edges.size();
+	}
 
-  /**
-   * Checks if two graphs are equal. Store all of Edges of larger Graph & Graphs are equal only if
-   * temp is unchanged *
-   */
-  public boolean equals(Graph<E> other) {
+	/**
+	 * Checks if two graphs are equal. Store all of Edges of larger Graph & Graphs are equal only if temp is unchanged *
+	 */
+	public boolean equals(Graph<E> other) {
 
-    if (other.edges.size() != edges.size())
-      return false;
+		if (other.edges.size() != edges.size())
+			return false;
 
-    ArrayList<Edge<E>> temp = new ArrayList<Edge<E>>(other.edges);
+		ArrayList<Edge<E>> temp = new ArrayList<Edge<E>>(other.edges);
 
-    return temp.retainAll(edges);
-  }
+		return temp.retainAll(edges);
+	}
+
+	public void reset() {
+		for (int i = 0; i < count(); i++)
+			get(i).setDistance(Integer.MAX_VALUE);
+	}
 }
--- src/main/java/graph/Link.java
+++ src/main/java/graph/Link.java
@@ -1,54 +1,37 @@
 package graph;
 
 public class Link<E> implements Comparable<Link<E>> {
+	private Edge<E> source, sink;
+	private int distance;
 
-  private Edge<E> source, sink;
-  private int distance;
+	public Link(Edge<E> source, Edge<E> sink, int distance) {
+		this.source = source;
+		this.sink = sink;
+		this.distance = distance;
+	}
 
-  /*
-   * constructor- creates a Link object to Connect two Nodes together with a standard distance of 0
-   * , it is assumed That distance is either weighted through the Edges or otherwise is irrelevant
-   */
-  public Link(Edge<E> source, Edge<E> sink) {
-    this(source, sink, 0);
-  }
+	public Edge<E> getSource() {
+		return source;
+	}
 
-  public Link(Edge<E> source, Edge<E> sink, int distance) {
-    this.source = source;
-    this.sink = sink;
-    this.distance = distance;
-  }
+	public Edge<E> getSink() {
+		return sink;
+	}
 
-  public Edge<E> getSource() {
-    return source;
-  }
+	public int getDistance() {
+		return distance;
+	}
 
-  public Edge<E> getSink() {
-    return sink;
-  }
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
 
-  public int getDistance() {
-    return distance;
-  }
+	@Override
+	public String toString() {
+		return "Link [source=" + source + ", sink=" + sink + ", distance=" + distance + "]";
+	}
 
-  public void setDistance(int distance) {
-    this.distance = distance;
-  }
-
-  /*
-   * Two connectors are equal if the two Edges are equal and the distance is equal
-   */
-  public boolean equals(Link<E> other) {
-    return source.equals(other.getSource()) && sink.equals(other.getSink())
-        && distance == other.getDistance();
-  }
-
-  @Override
-  public String toString() {
-    return "Link [source=" + source + ", sink=" + sink + ", distance=" + distance + "]";
-  }
-
-  public int compareTo(Link<E> other) {
-    return this.distance - other.distance;
-  }
+	public int compareTo(Link<E> other) {
+		return this.distance - other.distance;
+	}
 }
